9c55ab4e61f57ca69d5fd84ef597d859ea96e158
Igor Mianowany <fimiany@gmail.com>
2025-01-17 17:45:19 +0100

    fixed arrow spread bug, applied better formula

--- scripts/bow.gd
+++ scripts/bow.gd
@@ -2,7 +2,9 @@ class_name Bow
 extends WeaponType
 
 @export var attack_speed := .75
+@export var spread := 30
 var arrow_scene := preload("res://arrow.tscn")
+var rotation_change := 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,9 +13,13 @@ func _process(delta: float) -> void:
 func attack(damage : float, position : Vector2 = Vector2.ZERO, direction : Vector2 = Vector2.ZERO) -> void:
 	attack_started.emit()
 	var projectilesArray : Array[PackedScene]
+	var original_projectiles = projectiles
 	for index in projectiles:
 		var existing_direction = direction
-		var angle_in_radians = deg_to_rad(((index - projectiles/2)*45/projectiles))
+		# this is done so the arrows are moved based on the center, instead of just 45 degrees down or up
+		if projectiles > 1:
+			rotation_change = ((-spread/2) + ((spread/(projectiles-1)) * index))
+		var angle_in_radians = deg_to_rad(rotation_change)
 		var new_direction = existing_direction.rotated(angle_in_radians)
 
 		var projectile := arrow_scene.instantiate()
