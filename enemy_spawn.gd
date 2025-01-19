3b6b8cb1ef37b340fa38c098ef8b6f21235c2adc
Igor Mianowany <fimiany@gmail.com>
2025-01-19 12:42:48 +0100

    fixed enemy spawn, added spawn cooldown speedup with time

--- enemy_spawn.gd
+++ enemy_spawn.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @export var cooldown : float = 5
+@export var min_cooldown : float = 1
 @export var player : Player 
 @export var limit : int = 9999
 
@@ -15,8 +16,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Timer.wait_time = $Timer.wait_time + randf_range(0, 1)
-
+	pass
+	
 func _on_timer_timeout() -> void:
 	if count < limit:
 		var slime_instance = slime.instantiate()
@@ -24,3 +25,5 @@ func _on_timer_timeout() -> void:
 		slime_instance.global_position = global_position
 		add_child(slime_instance)
 		count += 1
+		cooldown = clampf(cooldown - player.get_elapsed_time() / 100, min_cooldown, cooldown)
+		$Timer.wait_time = cooldown + randf_range(0, 1)
