986226f0615a737c552124dc2ccdf5eba1592294
Igor Mianowany <fimiany@gmail.com>
2025-02-01 23:57:56 +0100

    fixed problem with filtering upgrades

--- upgrade_selection.gd
+++ upgrade_selection.gd
@@ -1,27 +1,28 @@
 extends Control
 
-@export var upgrades_amount : int = 100000
+@export var upgrades_amount : int = 3
 @onready var upgrade_container = $CanvasLayer/HBoxContainer
 
 func _ready() -> void:
 	var chosen_numbers : Array[int]
 	# just for testing
-	#TODO remove
-	if upgrades_amount > upgrade_container.get_children().size():
-		upgrades_amount = upgrade_container.get_children().size()
+	##TODO remove
+	#if upgrades_amount > upgrade_container.get_children().size():
+		#upgrades_amount = upgrade_container.get_children().size()
+	var upgrades : Array[Node] = $CanvasLayer/HBoxContainer.get_children()
 		
-	filter_upgrades()
+	filter_upgrades(upgrades)
 	
 	while chosen_numbers.size() < upgrades_amount:
-		var num = randi_range(0, upgrade_container.get_children().size()-1)
+		var num = randi_range(0,upgrades.size()-1)
 		if not chosen_numbers.has(num):
 			chosen_numbers.append(num)
-	for index in upgrade_container.get_children().size():
+	for index in upgrades.size():
 		if not chosen_numbers.has(index):
-			upgrade_container.get_children()[index].visible = false
+			upgrades[index].visible = false
 	PlayerState.level_up.connect(_upgrade_selection_visible)
 	get_tree().paused = true
-	for node in upgrade_container.get_children():
+	for node in upgrades:
 		node.upgrade_selected.connect(_quit)
 		
 func _input(event):
@@ -40,9 +41,14 @@ func _quit():
 func _upgrade_selection_visible():
 	add_child($CanvasLayer)
 	
-func filter_upgrades():
-	for upgrade in $CanvasLayer/HBoxContainer.get_children():
+func filter_upgrades(upgrades : Array[Node]):
+	for upgrade in upgrades:
 		if not (upgrade as UpgradeSelection).available():
-			$CanvasLayer/HBoxContainer.call_deferred("remove_child", upgrade)
+			#this monstrosity is because for some reason just removing the node
+			#from array is not enough and it's still being shown on screen,
+			#despite not being inside the array, so you cant even click it.
+			upgrade.visible = false
+			upgrades.remove_at(upgrades.rfind(upgrade))
+
 
 	
