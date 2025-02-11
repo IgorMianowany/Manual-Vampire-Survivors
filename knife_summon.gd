3a86d67c19f7ce5d8b524593a794a6c362fce6da
Igor Mianowany <fimiany@gmail.com>
2025-02-11 23:58:19 +0100

    better knife movement

--- knife_summon.gd
+++ knife_summon.gd
@@ -2,34 +2,33 @@ extends CharacterBody2D
 
 var target : Slime 
 var direction : Vector2
-var speed : float = 100
-@export var player : Player 
+var speed : float = 50
+var player : Player 
 @onready var timer : Timer = $Timer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("dupa")
 	timer.wait_time = 1
-	timer.one_shot = false
-	timer.start()
+	timer.autostart = true
+	player = owner
+	set_as_top_level(true)
+	global_position = player.global_position + Vector2(15,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	if target != null:
-		if global_position.distance_to(target.global_position) > 10:
-			velocity += speed * direction
-		else:
-			timer.start()
+	velocity = speed * direction
+	move_and_slide()
 
 
 func _on_timer_timeout() -> void:
-	print("dupa")
-	target = $TargetRange.get_overlapping_bodies()[0]
-	if global_position.distance_to(player.global_position) > 100:
+	target = null
+	var enemies = $TargetRange.get_overlapping_bodies()
+	for enemy in enemies:
+		target = enemy
+		break
+	if target == null or global_position.distance_to(player.global_position) > 100:
 		direction = global_position.direction_to(player.global_position)
-	elif target != null:
+	else:
 		direction = global_position.direction_to(target.global_position)
-	print("dupa2")
-	
