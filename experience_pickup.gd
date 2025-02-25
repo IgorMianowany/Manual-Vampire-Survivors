21d382762f1e4223fd810e959fb3e63d33def52b
Igor Mianowany <fimiany@gmail.com>
2025-02-25 08:11:19 +0100

    small refactor 2

--- experience_pickup.gd
+++ experience_pickup.gd
@@ -4,12 +4,10 @@ var player : Player
 var speed = 10000
 @export var experience_points = 1
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = true
 	collision_mask = 8
-	#set_as_top_level(true)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta: float) -> void:
 	if is_instance_valid(player):
 		velocity = (speed - global_position.distance_to(player.global_position)*10) * global_position.direction_to(player.global_position) * delta
@@ -19,8 +17,6 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		player = body
-		
-
 
 func _on_collision_range_body_entered(body: Node2D) -> void:
 	if body is Player:
