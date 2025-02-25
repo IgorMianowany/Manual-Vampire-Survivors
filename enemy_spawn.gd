70caee17da7da00b2cd36cfe680b9b1a843818a6
Igor Mianowany <fimiany@gmail.com>
2025-02-25 08:12:23 +0100

    small refactor 5

--- enemy_spawn.gd
+++ enemy_spawn.gd
@@ -7,13 +7,11 @@ extends Node2D
 var slime := preload("res://slime.tscn") 
 var count : int = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Timer.autostart = true
 	$Timer.wait_time = cooldown
 	$Timer.start()
 
-	
 func _on_timer_timeout() -> void:
 	if count < limit and PlayerState.slime_count < 50:
 		var slime_instance = slime.instantiate()
