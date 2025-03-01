9f9abffd0a730beb163abafb8a74be5c20c6062a
Igor Mianowany <fimiany@gmail.com>
2025-03-01 11:47:43 +0100

    added start menu logic impl

--- start_menu.gd
+++ start_menu.gd
@@ -1,11 +1,11 @@
 extends Control
+var level_scene := preload("res://level_01.tscn")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_quit_button_pressed() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_packed(level_scene)
