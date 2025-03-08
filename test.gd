d80353c7922885d1a9a16e3b57316032ac0aaa2c
Igor Mianowany <fimiany@gmail.com>
2025-03-08 18:34:46 +0100

    stat upgrade menu debugging #1

---
+++ test.gd
@@ -0,0 +1,16 @@
+extends Panel
+var texture : Texture2D = preload("res://icon.svg")
+@export var text : String
+@export var cost : int
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/MarginContainer2/TextureRect.texture = texture
+	$VBoxContainer/MarginContainer/Label.text = text
+	$VBoxContainer/MarginContainer3/Button.text = cost
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
